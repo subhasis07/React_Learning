05c02c6c7025b4cf7f08ed5a5b3cf424251dc5be
Subhasis Pal <palsubhasis100@gmail.com>
2024-07-29 23:35:59 +0530

    React-Learning Day-1

---
+++ Basics/script.js
@@ -0,0 +1,33 @@
+//
+
+const heading= React.createElement(
+    "h1", {id:"attributes"}, "Hello World from React (CDN link)"); //tag , attributes , childs --> this return an Obj
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading) //render help to convert the obj & then put it in html
+
+/*
+<div>
+    <h1>
+        <h2> inside parent div </h2>
+    </h1>
+
+</div>
+*/
+
+const box= React.createElement(
+    "h1", {id:"attributes"},
+    [React.createElement("h2",{},"Inside Parent Div"),
+    React.createElement("h2",{},
+        React.createElement(
+            "h3",
+            {},
+            "Inside of sub h2"
+        )
+    )
+    ]
+);
+const rootBox= ReactDOM.createRoot(document.getElementById("box-2"));
+
+rootBox.render(box)
